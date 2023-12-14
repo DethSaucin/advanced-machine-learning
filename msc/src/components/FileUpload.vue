d5f8b1e69035d1d49701859be42a5baf448f57f0
DethSaucin <83595771+DethSaucin@users.noreply.github.com>
2023-12-13 20:55:07 -0500

    predictions / comments
    now shows top 3 predictions / colored comments

--- msc/src/components/FileUpload.vue
+++ msc/src/components/FileUpload.vue
@@ -32,73 +32,84 @@ export default {
     };
   },
   methods: {
-    // Method to load the TensorFlow model
+    // > Method to load the TensorFlow model
     async loadModel() {
       try {
-        this.isLoading = true;  // Indicate that model loading is in progress
-        this.model = await tf.loadGraphModel('/model.json'); // Load the model from the specified path
-        this.isLoading = false; // Update loading status once the model is loaded
+        this.isLoading = true;  // > Indicate that model loading is in progress
+        this.model = await tf.loadGraphModel('/model.json'); // > Load the model from the specified path
+        this.isLoading = false; // > Update loading status once the model is loaded
       } catch (error) {
-        console.error('Error loading model:', error); // Log any errors during model loading
-        this.isLoading = false; // Ensure loading status is updated in case of an error
+        console.error('Error loading model:', error); // > Log any errors during model loading
+        this.isLoading = false; // > Ensure loading status is updated in case of an error
       }
     },
 
-    // Method to handle file selection from the input field
+    // > Method to handle file selection from the input field
     handleFileChange(event) {
-      this.audioFile = event.target.files[0]; // Store the selected audio file
-      this.audioFileName = this.audioFile ? this.audioFile.name : null; // Store the file name for display
-      this.audioUrl = this.audioFile ? URL.createObjectURL(this.audioFile) : null; // Create a URL for the audio file for playback
+      this.audioFile = event.target.files[0]; // > Store the selected audio file
+      this.audioFileName = this.audioFile ? this.audioFile.name : null; // > Store the file name for display
+      this.audioUrl = this.audioFile ? URL.createObjectURL(this.audioFile) : null; // > Create a URL for the audio file for playback
     },
 
-    // Method to analyze the genre of the selected audio file
+    // > Method to analyze the genre of the selected audio file
     async analyzeGenre() {
       if (!this.audioFile) {
-        alert('Please upload an audio file first.'); // Alert if no file is selected
+        alert('Please upload an audio file first.'); // > Alert if no file is selected
         return;
       }
-      this.isLoading = true; // Indicate that genre analysis is in progress
-      this.genre = null; // Reset the genre to null
-      const mfccs = await this.extractMFCCs(this.audioFile); // Extract MFCCs from the audio file
+      this.isLoading = true; // > Indicate that genre analysis is in progress
+      this.genre = null; // > Reset the genre to null
+      const mfccs = await this.extractMFCCs(this.audioFile); // > Extract MFCCs from the audio file
 
-      console.log('MFCCs shape:', mfccs.length); // Log the shape of the extracted MFCCs for debugging
+      console.log('MFCCs shape:', mfccs.length); // > Log the shape of the extracted MFCCs for debugging
 
       if (mfccs.length !== 13) {
-        console.error('Invalid MFCCs shape:', mfccs); // Check if the extracted features have the correct shape
-        this.isLoading = false; // Update loading status if features are invalid
+        console.error('Invalid MFCCs shape:', mfccs); // > Check if the extracted features have the correct shape
+        this.isLoading = false; // > Update loading status if features are invalid
         return;
       }
 
-      const prediction = this.model.predict(tf.tensor([mfccs])); // Predict the genre using the model
-      const predictedIndex = prediction.argMax(-1).dataSync()[0]; // Get the index of the highest probability genre
-      this.genre = this.genres[predictedIndex]; // Map the index to the corresponding genre name
-      this.isLoading = false; // Update loading status after prediction
+      const prediction = this.model.predict(tf.tensor([mfccs])); // > Predict the genre using the model
+      const predictionsArray = prediction.dataSync(); // > Convert prediction tensor to JavaScript array
+
+      // > Get the indices of the top 3 predictions
+      const topIndices = Array.from(predictionsArray)
+        .map((p, i) => ({ p, i }))
+        .sort((a, b) => b.p - a.p)
+        .slice(0, 3)
+        .map(a => a.i);
+
+      // > Map the indices to the corresponding genre names
+      this.genre = topIndices.map(index => this.genres[index]);
+
+      this.isLoading = false; // > Update loading status after prediction
     },
 
-    // Method to extract MFCCs from an audio file
+
+    // > Method to extract MFCCs from an audio file
     async extractMFCCs(file) {
-      // Create audio contexts for processing
+      // > Create audio contexts for processing
       const audioContext = new (window.AudioContext || window.webkitAudioContext)();
       const offlineAudioContext = new OfflineAudioContext(
-        1, // Number of channels
-        audioContext.sampleRate * 30, // Buffer size for 30 seconds of audio
-        audioContext.sampleRate // Sample rate
+        1, // > Number of channels
+        audioContext.sampleRate * 30, // > Buffer size for 30 seconds of audio
+        audioContext.sampleRate // > Sample rate
       );
 
-      // Decode the audio data from the file
+      // > Decode the audio data from the file
       const audioData = await file.arrayBuffer();
       const audioBuffer = await audioContext.decodeAudioData(audioData);
 
-      // Play the audio file for offline processing
+      // > Play the audio file for offline processing
       const bufferSource = offlineAudioContext.createBufferSource();
       bufferSource.buffer = audioBuffer;
       bufferSource.connect(offlineAudioContext.destination);
       bufferSource.start();
 
-      // Render the audio buffer for offline processing
+      // > Render the audio buffer for offline processing
       const renderedBuffer = await offlineAudioContext.startRendering();
 
-      // Set up Meyda analyzer to extract features from the rendered buffer
+      // > Set up Meyda analyzer to extract features from the rendered buffer
       let mfccs = [];
       let bufferSourceForMeyda = audioContext.createBufferSource();
       bufferSourceForMeyda.buffer = renderedBuffer;
@@ -109,7 +120,7 @@ export default {
         bufferSize: 2048,
         featureExtractors: ['mfcc'],
         callback: (features) => {
-          mfccs.push(features.mfcc); // Collect MFCCs
+          mfccs.push(features.mfcc); // > Collect MFCCs
         }
       });
 
@@ -118,30 +129,30 @@ export default {
 
       // > possible first solution
 
-      // Collect MFCCs for a short duration
-      // return new Promise(resolve => {
-      //   setTimeout(() => {
-      //     bufferSourceForMeyda.stop();
-      //     meydaAnalyzer.stop();
-      //     console.log("Collected MFCCs:", mfccs); // Log the collected MFCCs
-      //     resolve(this.averageFeatures(mfccs)); // Resolve the promise with averaged MFCCs
-      //   }, 10000); // Collect MFCCs for 10 seconds
-      // });
+      // > Collect MFCCs for a short duration
+      // > return new Promise(resolve => {
+      // >   setTimeout(() => {
+      // >     bufferSourceForMeyda.stop();
+      // >     meydaAnalyzer.stop();
+      // >     console.log("Collected MFCCs:", mfccs); // > Log the collected MFCCs
+      // >     resolve(this.averageFeatures(mfccs)); // > Resolve the promise with averaged MFCCs
+      // >   }, 10000); // > Collect MFCCs for 10 seconds
+      // > });
 
       // > possible second solution - testing
 
       return new Promise(resolve => {
-        // Resolve the promise as soon as audio playback ends
+        // > Resolve the promise as soon as audio playback ends
         bufferSourceForMeyda.onended = () => {
           console.log("Audio ended, resolving promise early.");
           meydaAnalyzer.stop();
-          console.log("Collected MFCCs:", mfccs); // Log the collected MFCCs
+          console.log("Collected MFCCs:", mfccs); // > Log the collected MFCCs
           resolve(this.averageFeatures(mfccs));
         };
 
-        // Set a timeout based on the audio duration or a maximum limit
-        const audioDuration = audioBuffer.duration * 1000; // Duration in milliseconds
-        const timeoutDuration = Math.min(audioDuration, 30000); // 30 seconds or audio duration, whichever is shorter
+        // > Set a timeout based on the audio duration or a maximum limit
+        const audioDuration = audioBuffer.duration * 1000; // > Duration in milliseconds
+        const timeoutDuration = Math.min(audioDuration, 30000); // > 30 seconds or audio duration, whichever is shorter
 
         setTimeout(() => {
           console.log("Timeout reached, resolving promise.");
@@ -153,19 +164,19 @@ export default {
 
 
     },
-    // Method to average the features (MFCCs)
+    // > Method to average the features (MFCCs)
     averageFeatures(featureArray) {
-      if (featureArray.length === 0) return []; // Return an empty array if no features are present
+      if (featureArray.length === 0) return []; // > Return an empty array if no features are present
 
-      let summedFeatures = featureArray[0].map(() => 0); // Initialize an array to store the summed features
+      let summedFeatures = featureArray[0].map(() => 0); // > Initialize an array to store the summed features
 
       featureArray.forEach(features => {
         features.forEach((feature, index) => {
-          summedFeatures[index] += feature; // Sum features across all frames
+          summedFeatures[index] += feature; // > Sum features across all frames
         });
       });
 
-      return summedFeatures.map(sum => sum / featureArray.length); // Average the features by dividing by the number of frames
+      return summedFeatures.map(sum => sum / featureArray.length); // > Average the features by dividing by the number of frames
     },
   },
   mounted() {
